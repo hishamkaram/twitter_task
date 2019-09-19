622017211fbeafeb9dc3951b4c3c56e550dd4013
hisham waleed karam <hisham.karam@cartologic.com>
2019-09-19 17:51:15 +0200

    add get tweets by hashtag view

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-"""twitter_task URL Configuration
+"""api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
-from api.urls import urlpatterns as api_urlpatterns
+from .views import get_tweets_by_hashtag
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hashtags/<str:hashtag>', get_tweets_by_hashtag),
 ]
-urlpatterns += api_urlpatterns
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-# from django.shortcuts import render
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import TweetSerializer
+from .twitter import twitter_api
+
+
+@api_view(['GET'])
+def get_tweets_by_hashtag(request, hashtag=None):
+    """Get Twitter Tweets By Hashtag.
+
+    Args:
+        request (django.http.HttpRequest):
+            django request object.
+        hashtag (str):
+            name of the hashtag.
+
+    Returns:
+        HttpReponse with list of hashtag tweets.
+
+    """
+    default_limit = settings.TWITTER_DEFAULT_LIMIT
+    limit = request.GET.get("limit", default_limit)
+    limit = int(limit)
+    tweets = twitter_api.get_hashtag_tweets(hashtag, limit)
+    serializer = TweetSerializer(tweets, many=True)
+    return Response(serializer.data)
