24717735a281ad2c457263211466ccfef2be9bf3
hisham waleed karam <hisham.karam@cartologic.com>
2019-09-20 13:45:55 +0200

    remove django admin urls

--- twitter_task/urls.py
+++ twitter_task/urls.py
@@ -13,12 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from api.urls import urlpatterns as api_urlpatterns
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-urlpatterns += api_urlpatterns
+urlpatterns = api_urlpatterns
