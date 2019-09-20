1534a1c98051ef765222a7d7d6ccb02bd91be115
hisham waleed karam <hisham.karam@cartologic.com>
2019-09-20 18:26:33 +0200

    add initial test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,44 @@
-# from django.test import TestCase
+from django.test import TestCase
+from api.twitter import TwitterApi, Account, Tweet, TwitterException
+from django.conf import settings
 
-# Create your tests here.
+
+class TwitterApiTestCase(TestCase):
+    def setUp(self):
+        key = settings.TWITTER_API_KEY
+        secret = settings.TWITTER_API_SECRET
+        self.twitter_api = TwitterApi(key, secret)
+        self.hashtag = "#nyc"
+        self.hashtag_failure = "!!!!"
+        self.screen_name = "AnyMindGroup"
+        self.screen_name_failure = "dummy_twitter"
+
+    def test_get_hashtag_tweets(self):
+        """Test get_hashtag_tweets success."""
+        hashtag_tweets = self.twitter_api.get_hashtag_tweets(self.hashtag)
+        count = len(hashtag_tweets)
+        self.assertNotEqual(count, 0)
+        self.assertLessEqual(count, settings.TWITTER_DEFAULT_LIMIT)
+        for tweet in hashtag_tweets:
+            self.assertIsInstance(tweet, Tweet)
+            self.assertIsInstance(tweet.account, Account)
+
+    def test_get_hashtag_tweets_failure(self):
+        """Test get_hashtag_tweets failure."""
+        with self.assertRaises(TwitterException):
+            self.twitter_api.get_hashtag_tweets(self.hashtag_failure)
+
+    def test_get_user_timeline(self):
+        """Test get_user_timeline success."""
+        user_tweets = self.twitter_api.get_user_timeline(self.screen_name)
+        count = len(user_tweets)
+        self.assertNotEqual(count, 0)
+        self.assertLessEqual(count, settings.TWITTER_DEFAULT_LIMIT)
+        for tweet in user_tweets:
+            self.assertIsInstance(tweet, Tweet)
+            self.assertIsInstance(tweet.account, Account)
+
+    def test_get_user_timeline_failure(self):
+        """Test get_hashtag_tweets failure."""
+        with self.assertRaises(TwitterException):
+            self.twitter_api.get_user_timeline(self.hashtag_failure)
