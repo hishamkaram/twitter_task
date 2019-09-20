4fb842fbd75f3d03e3fd3e3966248bd3b341f167
hisham waleed karam <hisham.karam@cartologic.com>
2019-09-20 19:47:57 +0200

    add api views test case

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,10 @@
-from django.test import TestCase
-from api.twitter import TwitterApi, Account, Tweet, TwitterException
 from django.conf import settings
+from django.shortcuts import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from api.twitter import Account, Tweet, TwitterApi, TwitterException
 
 
 class TwitterApiTestCase(TestCase):
@@ -42,3 +46,38 @@ class TwitterApiTestCase(TestCase):
         """Test get_hashtag_tweets failure."""
         with self.assertRaises(TwitterException):
             self.twitter_api.get_user_timeline(self.hashtag_failure)
+
+
+class ApiTestCase(APITestCase):
+    def setUp(self):
+        self.hashtag = "#nyc"
+        self.hashtag_failure = "!!!!"
+        self.screen_name = "AnyMindGroup"
+        self.screen_name_failure = "dummy_twitter"
+
+    def test_get_tweets_by_hashtag_view(self):
+        """Test get_tweets_by_hashtag view success."""
+        url = reverse('tweets-hashtag', kwargs={"hashtag": self.hashtag})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertLessEqual(len(response.data), settings.TWITTER_DEFAULT_LIMIT)
+        response = self.client.get(url, format='json', data={'limit': 50})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertLessEqual(len(response.data), 50)
+
+    def test_get_user_timeline_view_success(self):
+        """Test get_user_timeline view success."""
+        url = reverse('user-timeline', kwargs={"screen_name": self.screen_name})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertLessEqual(len(response.data), settings.TWITTER_DEFAULT_LIMIT)
+        response = self.client.get(url, format='json', data={'limit': 50})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertLessEqual(len(response.data), 50)
+
+    def test_get_user_timeline_view_failure(self):
+        """Test get_user_timeline view failure."""
+        url = reverse('user-timeline', kwargs={"screen_name": self.screen_name_failure})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn("error", response.data)
